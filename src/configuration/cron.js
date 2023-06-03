aba940eacea51a8483fc1cf7e59db9a7741f2f7c
anmolmathur5198 <mathur.anmol@dotsquares.com>
2023-06-03 11:03:01 +0530

    Create a cron for refresh the SessionId of Trams server for tully project

--- src/configuration/cron.js
+++ src/configuration/cron.js
@@ -1,14 +1,15 @@
 var CronJob = require("cron").CronJob;
+const { default: axios } = require("axios");
 const croncontroller = require("../controller/croncontroller");
-
 const { userregister } = require("../models");
 const later = require("later");
 const moment = require("moment");
+const Tokens = require("../models/tokens");
 exports.runCron = () => {
   var job = new CronJob(
     "*/6 * * * *",
     async function () {
-     
+
 
       let users = await userregister.find().lean();
       if (users.length > 0) {
@@ -132,7 +133,7 @@ exports.runCron = () => {
 
           // cron.syncZohoProducts(user).then(async (result) => {
           //   console.log(`Success - product sync to sync for  ${user.email}`);
-           
+
           //   await userregister.updateOne({ _id: user._id }, { cronSyncingBusyStatusProduct: false });
 
 
@@ -145,5 +146,27 @@ exports.runCron = () => {
     null,
     true
   ).start();
+
+  //  This cron is used for refresh the sessionId  which returns by the trams server of login API we have to store this sessionId into MongoDB for future reference. (Nischay Jain)
+  new CronJob("* */5 * * *", async () => {
+    try {
+      const body = {
+        "username": process.env.USER_NAME,
+        "password": process.env.PASSWORD,
+        "alias": process.env.ALIAS,
+        "appType": process.env.APP_TYPE,
+        "accessKey": process.env.ACCESS_KEY
+      }
+      const { data } = await axios.get("http://tlt-dev01:8085/login", { params: body }, { 'headers': { "Content-Type": "application/json" } })
+      const SessionID = data.result.SessionID;
+      const user_id = "647895e3c475dfb6a1049628"
+      const response = await Tokens.findOneAndUpdate({ tokenname: "TramsSessionId" }, { $set: { tokenname: "TramsSessionId", platform: "tully", user_id, access_token: SessionID } }, { upsert: true });
+      console.log("Token Updated in MongoDB, Token's Table:- ", response);
+    }
+    catch (error) {
+      console.log("catch block executed", error);
+    }
+
+  }).start();
 
 };
